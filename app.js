3e7a50f20b50d22e6fdbf683dc1ed7dee1ba000c
Matt <matthewbrion@me.com>
2026-09-01 10:12:43 -0400

    wired up app.js

--- app.js
+++ app.js
@@ -1,3 +1,18 @@
 import express from "express";
+import usersRouter from '#api/usersRouter';
+import productsRouter from '#api/productsRouter';
+import ordersRouter from '#api/ordersRouter';
+
 const app = express();
+app.use(express.json());
+
+app.use('/users', usersRouter);
+app.use('/products', productsRouter);
+app.use('/orders', ordersRouter);
+
+app.use((err, req, res, next) => {
+    console.error(err);
+    res.status(err.status || 500).send(err.message || 'Sorry, something went wrong');
+});
+
 export default app;
